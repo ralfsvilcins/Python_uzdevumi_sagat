7f75f9ab85e67e8580d90d775d3795e7726e5724
ralfsvilcins <ralfs.v06@gmail.com>
2023-11-03 14:46:54 +0200

    izpildiju 3. uzd

--- 3uzd.py
+++ 3uzd.py
@@ -5,3 +5,10 @@ piemēram, vārdu, vecumu un ceļojumam iemaksāto summu.
 
 """
 
+class Dati:
+    def __init__(self, vards, vecums, iem_summa):
+        self.vards=vards
+        self.vecums=vecums
+        self.iem_summa=iem_summa
+
+kkk=Dati("Pēteris", 20, 0.5)
