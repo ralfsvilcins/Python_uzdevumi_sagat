ae429b0b98f00978716c071be64b09ed9a0fabdb
ralfsvilcins <ralfs.v06@gmail.com>
2023-11-03 14:39:35 +0200

    izpildiju 2. uzd

--- 2uzd.py
+++ 2uzd.py
@@ -5,3 +5,20 @@ kas ir RSS?
 
 
 """
+
+import feedparsen
+
+# URL uz RSS plūsmu 
+
+rss_ur1='https://news.google.com/home?hl=lv&gl=LV&ceid=LV:lv'
+
+# iegūsim RSS plūsmas datus
+kkk=feedparsen.parse(rss_ur1)
+
+#parbauda un janoforme 
+
+for entry in kkk.entries:
+    print("Virsraksts: ", entry.title)
+    print("Saite: ", entry.link)
+    print("Publicēšanas datums:", entry.published)
+    print('\n')
